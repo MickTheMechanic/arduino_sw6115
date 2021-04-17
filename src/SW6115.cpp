171daf8b71d4bef3ff567c49bece614e50bef28a
MickTheMechanic <44751666+MickTheMechanic@users.noreply.github.com>
2021-04-17 11:59:16 +0200

    Update SW6115.cpp

--- src/SW6115.cpp
+++ src/SW6115.cpp
@@ -1,1 +1,25 @@
+#include "SW6115.h"
 
+SW6115::SW6115(uint8_t address, TwoWire& bus) {
+	_deviceAddr = address;
+	_bus = &bus;
+}
+
+SW6115::~SW6115() {}
+
+float SW6115::Vbatt()
+{
+	_bus->beginTransmission(_deviceAddr);
+	_bus->write(ADC_VDATA_1);
+	_bus->endTransmission();
+	_bus->requestFrom(_deviceAddr, (uint8_t)1);
+	uint8_t r14 = _bus->read();
+  _bus->beginTransmission(_deviceAddr);
+	_bus->write(ADC_VDATA_2);
+	_bus->endTransmission();
+	_bus->requestFrom(_deviceAddr, (uint8_t)1);
+  uint8_t r15 = _bus->read();
+  return (((r15 << 8) | r14) & 0xFFF) * 0.0012;
+  
+}
+#endif
