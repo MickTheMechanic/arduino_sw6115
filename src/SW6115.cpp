66b270f79e8f1d166e28bedc441341ddcace5df6
MickTheMechanic <44751666+MickTheMechanic@users.noreply.github.com>
2021-04-19 20:59:18 +0200

    Update SW6115.cpp

--- src/SW6115.cpp
+++ src/SW6115.cpp
@@ -38,7 +38,7 @@ float SW6115::readVout()
         return (((r15 << 8) | r16) & 0xFFF) * 4;
 	//Vout= ((Reg0x15[7:4]<<8)+ Reg0x16[7:0]) *4 mV
 }
-float SW6115::readIchrge()
+float SW6115::readIchrg()
 {
 	_bus->beginTransmission(_deviceAddr);
 	_bus->write(static_cast<uint8_t>(SW6115Register::ADC_IDATA_1));
